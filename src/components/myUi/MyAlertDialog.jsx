06f55a1ed5d0498f7e6e26eba39c3726bd62492a
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2024-12-08 10:51:06 +0500

    replace trigger with btn

--- src/components/myUi/MyAlertDialog.jsx
+++ src/components/myUi/MyAlertDialog.jsx
@@ -9,13 +9,14 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { Button } from "../ui/button";
 
 export default function MyAlertDialog({ children, triggerText }) {
   const child = Array.isArray(children) ? children[1] : children;
   return (
     <AlertDialog>
-      <AlertDialogTrigger className="bg-primary text-primary-foreground text-sm shadow hover:bg-primary/90 rounded-lg h-9 px-4 py-2">
-        {triggerText}
+      <AlertDialogTrigger asChild>
+        <Button>{triggerText}</Button>
       </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
